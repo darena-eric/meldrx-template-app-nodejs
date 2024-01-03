50dcb11c2f31f3915aa82f942054e2d91fe87be1
darena-eric <eric@darenasolutions.com>
2024-01-02 22:34:34 -0600

    Use workspace URL

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,15 @@ require("dotenv").config();
 
 const AUTH_URL = process.env.MELDRX_AUTH_URL ?? "";
 const CLIENT_ID = process.env.MELDRX_CLIENT_ID ?? "";
-const WORKSPACE_ID = process.env.MELDRX_WORKSPACE_ID ?? "";
+const WORKSPACE_URL = process.env.MELDRX_WORKSPACE_URL ?? "";
 const KID = "1234567890";
 const ALG = "RS384";
 const PRIVATE_KEY = fs.readFileSync(__dirname + "/../privatekey.pem").toString();
 
 async function main() {
-    const fhirUrl = MeldRxUtils.getWorkspaceFhirUrl(AUTH_URL, WORKSPACE_ID);
-    const tokenUrl = MeldRxUtils.getWorkspaceTokenUrl(AUTH_URL, WORKSPACE_ID);
+    const workspaceId = WORKSPACE_URL.split("/").pop() ?? "";
+    const fhirUrl = MeldRxUtils.getWorkspaceFhirUrl(AUTH_URL, workspaceId);
+    const tokenUrl = MeldRxUtils.getWorkspaceTokenUrl(AUTH_URL, workspaceId);
     const token = await getBackendAccessCodeForJWKS(PRIVATE_KEY, CLIENT_ID, tokenUrl, KID, ALG);
 
     const meldRxClient: MeldRxFhirClient = MeldRxFhirClient.forBearerToken(fhirUrl, token.access_token);
